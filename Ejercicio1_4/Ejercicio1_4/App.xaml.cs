3d361a36111a9089f5401b356334423cdd61459b
Joshua Mclean <86990609+JoshuaMc1@users.noreply.github.com>
2022-10-10 15:52:38 -0500

    Initial commit

---
+++ Ejercicio1_4/Ejercicio1_4/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Ejercicio1_4.Controlador;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Ejercicio1_4
+{
+    public partial class App : Application
+    {
+        static PersonaControlador database;
+
+        public static PersonaControlador Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new PersonaControlador(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "dbPersona.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Ejercicio1_4/Ejercicio1_4/Vista/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Ejercicio1_4.Modelo;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using Xamarin.Forms;
+
+namespace Ejercicio1_4
+{
+    public partial class MainPage : ContentPage
+    {
+        MediaFile foto = null;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void txtTomarFoto_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    mensaje("Error", "No se han otorgado los permisos para suar la camara o la camara no esta disponible.");
+                    return;
+                }
+                foto = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions());
+                if (foto != null)
+                {
+                    txtImagen.Source = ImageSource.FromStream(() =>
+                    {
+                        return foto.GetStream();
+                    });
+                }
+                else return;
+            } catch (Exception ex)
+            {
+                mensaje("Error", "Error: " + ex.Message.ToString());
+            }
+        }
+
+        public bool validarCampoVacio(Entry campo)
+        {
+            return String.IsNullOrEmpty(campo.Text);
+        }
+
+        public async void mensaje(String tittulo, String mensaje)
+        {
+            await DisplayAlert(tittulo, mensaje, "Ok");
+        }
+
+        private async void txtGuardarFoto_Clicked(object sender, EventArgs e)
+        {
+            if (foto != null)
+            {
+                if (!validarCampoVacio(txtDescripcion) && !validarCampoVacio(txtNombre))
+                {
+                    var persona = new Persona
+                    {
+                        descripcion = txtDescripcion.Text.Trim(),
+                        nombre = txtNombre.Text.Trim(),
+                        foto = foto.Path
+                    };
+
+                    int result = await App.Database.guardarPersona(persona);
+                    if (result > 0)
+                    {
+                        mensaje("Ingreso exitoso", "Los datos se guardaron exitosamente");
+                        limpiarCampos();
+                    }
+                    else mensaje("Error al guardar", "Al parecer los datos no se han guardado");
+                }
+                else mensaje("Error", "Debe llenar todos los campos solicitados.");
+            }
+            else mensaje("Error", "Debe tomar una foto");
+        }
+
+        public void limpiarCampos()
+        {
+            txtDescripcion.Text = "";
+            txtNombre.Text = "";
+            txtImagen.Source = "https://cdn.pixabay.com/photo/2022/10/04/20/32/gears-7499058_960_720.png";
+        }
+
+        private async void btnVerLista_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Vista.Lista());
+        }
+    }
+}
